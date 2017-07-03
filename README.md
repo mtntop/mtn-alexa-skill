96a0c08be2312bb9e3ff9eef681ec2134db3819f
Tyler Leonhardt <tylerl0706@gmail.com>
2017-07-03 10:54:22 -0700

    use of the API

---
+++ README.md
@@ -0,0 +1,11 @@
+# "Is the mountain out?" - Alexa Skill edition
+
+An Alexa Skill that answers a simple - yet important - question: "Is the mountain out?"
+
+If you're not from Seattle and don't know what I mean by Mountain, this is what I'm talking about!:
+
+![The Mountain is Out!](http://media-cache-ec0.pinimg.com/736x/32/0d/cd/320dcdbdcb002671cd13b0641e3cfbe5.jpg)
+
+Mt Rainier in all its glory :)
+
+You can get this Skill [here](https://www.amazon.com/Tyler-Leonhardt-Mt-Rainier/dp/B072KGC5B1/ref=sr_1_1?s=digital-skills&ie=UTF8&qid=1497715426&sr=1-1&keywords=Mt+Rainier)!
---
+++ index.js
@@ -0,0 +1,39 @@
+const alexaSkillKit = require('alexa-skill-kit');
+const AlexaMessageBuilder = require('alexa-message-builder');
+const request = require('request-promise');
+const imgur = require('imgur');
+
+imgur.setClientId('31f3d3012240062');
+imgur.setAPIUrl('https://api.imgur.com/3/');
+
+exports.handler = function(event, context) {
+  alexaSkillKit(event, context, parsedMessage => {
+    let config = {
+      uri: 'https://themtn.top/api',
+      simple: false,
+      json: true
+    }
+
+    let verdict;
+
+    return request.get(config)
+      .catch(err => {
+        console.log('caught error', err.stack)
+      })
+      .then(result => {
+        verdict = result.result ? 'The mountain is out!' : 'The mountain is not out.';
+
+        return imgur.uploadUrl(result.image);
+      })
+      .then(result => {
+        console.log(result);
+        return new AlexaMessageBuilder()
+          .addText(verdict)
+          .addStandardCard('Mt.Rainier', verdict, {
+            smallImageUrl: result.data.link.replace('http://', 'https://'),
+            largeImageUrl: result.data.link.replace('http://', 'https://')
+          })
+          .get()
+      });
+  })
+}
